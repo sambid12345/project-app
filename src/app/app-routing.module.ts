83b91e9c1cd4b4a360614320c62d4a59ba44e550
sambid <sambid.champati@infosys.com>
2022-07-06 00:07:15 +0530

    main: separate feature module added for Recipe Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/auth.guard.service';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesResolverService } from './recipes/recipes.resolver.service';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
 	{path: '' , redirectTo: '/recipes', pathMatch: 'full'},
-	{path: 'recipes', component: RecipesComponent, children:[
-		{path: '', component: RecipeStartComponent},
-		{path: 'new', component: RecipeEditComponent},
-		{path:':id', component: RecipeDetailComponent, resolve:[RecipesResolverService]},
-		{path: ':id/edit', component: RecipeEditComponent, resolve:[RecipesResolverService]}
-	],
-	canActivate: [AuthGuard]},
 	{path: 'shopping-list', component: ShoppingListComponent},
 	{path: 'auth', component: AuthComponent}
-
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forChild(routes)],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AlertComponent } from './shared/alert/alert.component';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HeaderComponent,
-		RecipesComponent,
-		RecipeListComponent,
-		RecipeDetailComponent,
-		RecipeItemComponent,
+		HeaderComponent,		
 		ShoppingListComponent,
 		ShoppingEditComponent,
-		RecipeStartComponent,
-		RecipeEditComponent,
 		AuthComponent,
 		LoadingSpinnerComponent,
 		AlertComponent
@@ -41,7 +29,8 @@ import { AlertComponent } from './shared/alert/alert.component';
 		AppRoutingModule,
 		ReactiveFormsModule,
 		FormsModule,
-		HttpClientModule
+		HttpClientModule,
+		RecipesModule
 	],
 	providers: [ShoppingListService, RecipeService],
 	bootstrap: [AppComponent]
