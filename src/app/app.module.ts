719c4925e9f07e5f184251ce0d1491cf28f34c81
sambid <sambid.champati@infosys.com>
2022-07-08 23:28:22 +0530

    main: Auth Module separated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,			
-		AuthComponent,
 	],
 	imports: [
 		BrowserModule,
@@ -26,7 +23,8 @@ import { SharedModule } from './shared/shared.module';
 		HttpClientModule,
 		RecipesModule,
 		ShoppingListModule,
-		SharedModule
+		SharedModule,
+		AuthModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
 import { ShoppingListComponent } from "./shopping-list.component";
