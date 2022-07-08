415f470f6e3258be693acb8580ad3c1ae36a8df7
sambid <sambid.champati@infosys.com>
2022-07-09 00:10:41 +0530

    main: routing fix added

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
 export class RecipesRoutingModule{
